55e5790fc90afcaada55f363b43f0053132a6192
KOldland <kris@1927media.com>
2026-03-07 18:24:20 +0000

    Revert "test(khm): add promo checkout spy coverage for stripe discount wiring"
    This reverts commit 72e0fb35be75ea2db9b15b5030cf6f509ba78f94.

--- app/public/wp-content/plugins/khm-plugin/src/Rest/CheckoutController.php
+++ app/public/wp-content/plugins/khm-plugin/src/Rest/CheckoutController.php
@@ -38,7 +38,11 @@ class CheckoutController {
             return new WP_REST_Response([ 'message' => __( 'Invalid membership level.', 'khm-membership' ) ], 400);
         }
 
-        $level = $this->resolve_membership_level( $levelId, $request );
+        if ( ! function_exists('khm_get_membership_level') ) {
+            return new WP_REST_Response([ 'message' => __( 'Membership system unavailable.', 'khm-membership' ) ], 500);
+        }
+
+        $level = khm_get_membership_level($levelId);
         if ( ! $level ) {
             return new WP_REST_Response([ 'message' => __( 'Membership level not found.', 'khm-membership' ) ], 404);
         }
@@ -161,18 +165,6 @@ class CheckoutController {
         }
 
         return null;
-    }
-
-    /**
-     * @return mixed
-     */
-    protected function resolve_membership_level( int $levelId, WP_REST_Request $request ) {
-        $level = null;
-        if ( function_exists( 'khm_get_membership_level' ) ) {
-            $level = khm_get_membership_level( $levelId );
-        }
-
-        return apply_filters( 'khm_checkout_membership_level_override', $level, $levelId, $request );
     }
 
     private function resolve_allow_promotion_codes( int $levelId ): bool {
